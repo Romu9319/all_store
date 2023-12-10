44d97ebd9dc7df87dc7ef575bc87ef75f5ae7504
Romu9319 <danielerm1510@gmail.com>
2023-12-10 22:28:34 +0100

    apiSTORE and index templated added

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 
+import requests
+
 # Create your views here.
 """Vista catalogo de productos"""
 
 def index(request):
-    return render(request,"index.html")
+    url_products = "https://fakestoreapi.com/products"
+    response = requests.get(url_products)
+
+    if response.status_code == 200:
+        categories = []
+        data = response.json() 
+
+        for products in data:
+            if products["category"] not in categories:
+                categories.append(products["category"] )
+
+        context = {
+            "data": data,
+            "categories": categories
+        }      
+
+        return render(request, "index.html", context)
+    
+    else: 
+        return render(request, "index.html", {'error_message': f"Error al obtener datos: {response.status_code}"})
+    
+
+
